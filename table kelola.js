76b25b1f67df526e30cc3935b4c0b1b99c0471f6
unknown <salimfawwazmuwaffaq@gmail.com>
2020-12-26 11:51:37 +0700

    initial commit

---
+++ table kelola.js
@@ -0,0 +1,14 @@
+// table
+$(".delete1").click(function(){
+    $('#myTableRow1').remove();
+})
+
+// table
+$(".delete2").click(function(){
+    $('#myTableRow2').remove();
+})
+
+// table
+$(".delete3").click(function(){
+    $('#myTableRow3').remove();
+})
